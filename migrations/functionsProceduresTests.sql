b217f1dd186fe7d26e1a05824f7681aa21b41da3
Maksymilian Siemienowicz <siemienowicz1@gmail.com>
2023-06-13 20:43:53 +0200

    Gotowy projekt

--- migrations/functionsProceduresTests.sql
+++ migrations/functionsProceduresTests.sql
@@ -13,3 +13,13 @@ WHERE id = 1;
 
 
 SELECT * FROM airport.Flight_History;
+
+SELECT * FROM airport_LeftJoinView;
+SELECT * FROM airport_RightJoinView;
+SELECT * FROM airport_InnerJoinView;
+SELECT * FROM airport.FullOuterJoinView;
+SELECT * FROM airport_UnionView;
+SELECT * FROM airport.IntersectView;
+SELECT * FROM airport.ExceptView;
+SELECT * FROM airport.Flight_Details;
+SELECT * FROM airport.Passenger_Details;
